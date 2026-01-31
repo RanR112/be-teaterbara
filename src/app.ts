99bc48b8abdc5a1880b6c97dde32aedc3f66edea
RanR112 <146650544+RanR112@users.noreply.github.com>
2026-02-01 00:10:47 +0700

    modified cors configuration

--- src/app.ts
+++ src/app.ts
@@ -4,28 +4,12 @@ import galleryRoutes from "./routes/gallery.routes";
 
 const app = express();
 
-const allowedOrigins = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "https://teaterbara.my.id",
-    "https://www.teaterbara.my.id",
-];
+app.use(cors());
 
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin) return callback(null, true);
-
-            if (allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    }),
-);
+app.use((req, res, next) => {
+    res.setHeader("Cache-Control", "no-store");
+    next();
+});
 
 app.use(express.json());
 
