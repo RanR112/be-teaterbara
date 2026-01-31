e5346d3e303cab6ed89920f5ac39450ceec8ddcf
RanR112 <146650544+RanR112@users.noreply.github.com>
2026-02-01 00:06:36 +0700

    delete credentials on cors

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,6 @@ app.use(
         },
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true,
     }),
 );
 
